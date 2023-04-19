fce3c0758990cdb39b0fea5a4ba3a7a5d0f20508
MarcoInc <soteramarco@gmail.com>
2023-04-19 18:29:53 +0200

    add js tracks

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,5 @@
-console.log("ciao");
+let tracks=[{ 'name' : 'track 1', 'id':'pop' , 'artist': 'artista1' },
+            { 'name' : 'track 2', 'id':'rap' , 'artist': 'artista2' },
+            { 'name' : 'track 3', 'id':'rock' , 'artist': 'artista3' },
+            { 'name' : 'track 4', 'genre':'classic' , 'artist': 'artista4' }
+]
