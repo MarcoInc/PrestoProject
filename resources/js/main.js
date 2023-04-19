2b1b42eb757a9bf0af83e4facd050b9b1f6cd50d
MarcoInc <soteramarco@gmail.com>
2023-04-19 18:51:43 +0200

    add trasparenza navbar

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,3 +3,4 @@ let tracks=[{ 'name' : 'track 1', 'id':'pop' , 'artist': 'artista1' },
             { 'name' : 'track 3', 'id':'rock' , 'artist': 'artista3' },
             { 'name' : 'track 4', 'genre':'classic' , 'artist': 'artista4' }
 ]
+
