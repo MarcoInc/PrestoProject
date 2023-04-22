7e827f5f0e1e25264a0728308b00ca6f26646ef5
Miguel De la Vega <miguelangel.dlvn@gmail.com>
2023-04-22 15:58:22 +0200

    counter

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,3 +4,39 @@ let tracks=[{ 'name' : 'track 1', 'id':'pop' , 'artist': 'artista1' },
             { 'name' : 'track 4', 'genre':'classic' , 'artist': 'artista4' }
 ]
 
+
+
+// counter
+
+let firstnumber = document.querySelector ('#firstnumber')
+let secondnumber = document.querySelector("#secondnumber");
+let thirdnumber = document.querySelector("#thirdnumber");
+
+let counter = 0;
+
+function interval(element, finalnumber, frame) {
+    let interval = setInterval(() => {
+      if (counter < finalnumber) {
+        counter++;
+        element.innerHTML = counter;
+      } else {
+        clearInterval();
+      }
+    }, frame);
+}
+
+    interval(firstnumber, 1000, 1);
+    interval(secondnumber, 5000, 1);
+    interval(thirdnumber, 7000, 1);
+// intersection observer
+// let observer = new IntersectionObserver ((entries) => {
+//     entries.forEach(entry =>{
+//         if (entry.isIntersecting) {
+//             interval(firstnumber, 1000, 1);
+//             interval(secondnumber, 5000, 1);
+//             interval(thirdnumber, 7000, 1);
+//         }
+//     })
+// })
+
+
