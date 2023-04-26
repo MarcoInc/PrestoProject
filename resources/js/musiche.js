889de0fa3c9af9942fc71a5d396e1903c6478bbd
MarcoInc <soteramarco@gmail.com>
2023-04-26 17:22:38 +0200

    single filter added

--- resources/js/musiche.js
+++ resources/js/musiche.js
@@ -8,7 +8,6 @@ fetch('../obj/musica.json')
   let radioArtista=document.querySelector('#radioArtista');
   let radioAnno=document.querySelector('#radioAnno');
 
-
   //genera radio button con generi univoci
   function setGenere(){
     let generi=[]; //generi univoci
@@ -35,7 +34,7 @@ fetch('../obj/musica.json')
       let div=document.createElement('div');
       div.classList.add('form-check');
       div.innerHTML=` 
-          <input class="form-check-input" type="radio" name="genere" id="${genere}">
+          <input class="form-check-input genere" type="radio" name="genere" id="${genere}">
             <label class="form-check-label" for=${genere}>
         ${genere} (${count})
         </label>
@@ -71,7 +70,7 @@ fetch('../obj/musica.json')
       let div=document.createElement('div');
       div.classList.add('form-check');
       div.innerHTML=` 
-          <input class="form-check-input" type="radio" name="artista" id="${artista}">
+          <input class="form-check-input artista" type="radio" name="artista" id="${artista}">
             <label class="form-check-label" for=${artista}>
         ${artista} (${count})
         </label>
@@ -107,7 +106,7 @@ fetch('../obj/musica.json')
       let div=document.createElement('div');
       div.classList.add('form-check');
       div.innerHTML=` 
-          <input class="form-check-input" type="radio" name="anno" id="${anno}">
+          <input class="form-check-input anno" type="radio" name="anno" id="${anno}">
             <label class="form-check-label" for=${anno}>
         ${anno} (${count})
         </label>
@@ -117,6 +116,143 @@ fetch('../obj/musica.json')
   }
   setAnno(); //eseguo la funzione precedente
 
+  //crea le cards
+  let containerCards=document.querySelector('#containerCards');
+  function createCards(array) {
+    // Ripulire il contenuto della sezione
+    containerCards.innerHTML = '';
+    array.forEach(musica => {
+        let div = document.createElement('div');
+        div.classList.add('card', 'mb-2', 'mx-2');
+        div.style.width='18rem';
+        div.innerHTML = `
+          <img src="${musica.cover}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${musica.title}</h5>
+            <p class="card-text">${musica.artist}</p>
+            <p class="card-title">${musica.year}</p>
+            <p class="card-title">${musica.price}€</p>
+            <p class="card-title">(${musica.genre})</p>
+            <a href="#" class="btn riproduci">play</a>
+          </div>
+        `;
+        containerCards.appendChild(div);
+    })
+}
+// Invocare la funzione createCards per creare gli Annunci
+createCards(data);
 
+
+  let inputRange=document.querySelector('#inputRange'); //slider range
+  let maxValue=document.querySelector('#maxValue'); //valore max
+  //trova il prezzo maggiore
+  function rangeMax(){
+    let prices = data.map(musica => Number(musica.price)); // crea un array con i soli price di data
+    let maxPrice = Math.max(...prices); //trova il max in un array di numeri con lo spread operator
+    console.log(maxPrice);
+    inputRange.max = maxPrice; //accede al parametro max del range
+    inputRange.value = maxPrice; //accede al parametro min del range
+    maxValue.innerHTML = `${maxPrice}&euro;`//popolo il container
+  }
+  rangeMax();
+
+//--------------FILTRO GENERE------------------
+let radioGenreInput = document.querySelectorAll('.genere');
+function filterByGenere(genre) {
+    let filtered = data.filter(musica => musica.genre == genre);
+    createCards(filtered);
+  }
+
+  radioGenreInput.forEach(radioButton => {
+      radioButton.addEventListener('click', () => {
+          // console.log(radioButton.id);
+          let genereSelected = radioButton.id;
+          // Selezionato Tutte le Categorie
+          if (genereSelected == 'AllGenres') {
+              // Invocare la funzione createCards
+              createCards(data);
+          } else {
+            filterByGenere(genereSelected);
+          }
+      })
+  })
+
+  //--------------FILTRO ARTISTA------------------
+let radioArtistInput = document.querySelectorAll('.artista');
+function filterByArtista(artist) {
+    let filtered = data.filter(musica => musica.artist == artist);
+    createCards(filtered);
+  }
+
+  radioArtistInput.forEach(radioButton => {
+      radioButton.addEventListener('click', () => {
+          // console.log(radioButton.id);
+          let artistSelected = radioButton.id;
+          // Selezionato Tutte le Categorie
+          if (artistSelected == 'AllArtists') {
+              // Invocare la funzione createCards 
+              createCards(data);
+          } else {
+            filterByArtista(artistSelected);
+          }
+      })
+  })
+
+    //--------------FILTRO ANNO------------------
+let radioYearInput = document.querySelectorAll('.anno');
+function filterByAnno(year) {
+    let filtered = data.filter(musica => musica.year == year);
+    createCards(filtered);
+  }
+
+  radioYearInput.forEach(radioButton => {
+      radioButton.addEventListener('click', () => {
+          // console.log(radioButton.id);
+          let yearSelected = radioButton.id;
+          // Selezionato Tutte le Categorie
+          if (yearSelected == 'AllYears') {
+              // Invocare la funzione createCards 
+              createCards(data);
+          } else {
+            filterByAnno(yearSelected);
+          }
+      })
+  })
+
+
+  //--------------------FILTRO PREZZO----------------
+//filtra per prezzo
+  function filterByPrice(number) {
+    let filtered = data.filter(annuncio => Number(annuncio.price) <= Number(number));
+    createCards(filtered);
+  }
+
+  let numberPrice=document.querySelector('#valueSelected'); //valore selezionato che sta nel mezzo
+
+  //cattura il value del range ad ogni modifica del value
+  inputRange.addEventListener('input', () => {
+    //console.log(inputRange.value); 
+    filterByPrice(inputRange.value);
+    numberPrice.innerHTML = `${inputRange.value}&euro;`
+  })
+
+    //contiene la stringa nel form
+  let searchWord = document.querySelector('#searchWord');
+
+
+  //--------------FILTRO FORM------------------------
+  //aggiorna il valore del form ad ogni input
+  searchWord.addEventListener('input', () => {
+      //console.log( searchWord.value );
+      filterByWord(searchWord.value);
+  })
+  //ricerca la parola inserita
+  function filterByWord(word) {
+    let filtered;
+    //se esiste la parola cercata mettila in filtered
+    filtered = data.filter(element => element.title.toLowerCase().includes(word.toLowerCase()));
+     createCards(filtered);
+  }
 
 })
+
